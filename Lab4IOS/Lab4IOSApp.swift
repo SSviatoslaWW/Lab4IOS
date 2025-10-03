c8300299190ce0a26df244c764ff33b114583a77
SSviatoslaWW <sviatoslaw7779@gmail.com>
2025-10-03 12:18:49 +0300

    Initial Commit

---
+++ Lab4IOS/Lab4IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab4IOSApp.swift
+//  Lab4IOS
+//
+//  Created by Svituk on 03.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Lab4IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab4IOSTests/Lab4IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab4IOSTests.swift
+//  Lab4IOSTests
+//
+//  Created by Svituk on 03.10.2025.
+//
+
+import Testing
+@testable import Lab4IOS
+
+struct Lab4IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
