d7c515af50a2bb6fb39d4b2a4d7b98d51c9ac59e
SSviatoslaWW <sviatoslaw7779@gmail.com>
2025-10-15 14:17:48 +0300

    refactoring code

--- Lab4IOS/ContentView.swift
+++ Lab4IOS/ContentView.swift
@@ -5,34 +5,33 @@ struct ContentView: View {
     @State private var nameTextField: String = ""
     @State private var surenameTextField: String = ""
     
-    private let darkGradient = [
-        Color.black,
-        Color(red: 202/255, green: 138/255, blue: 4/255),
-        Color(red: 250/255, green: 204/255, blue: 21/255)
-    ]
-    private let darkTextColor = Color.white
+    private let darkTheme = ThemeColor(
+        gradientBg: [
+            Color.black,
+            Color(red: 202/255, green: 138/255, blue: 4/255),
+            Color(red: 250/255, green: 204/255, blue: 21/255)
+        ],
+        textColor: .white,
+        textFieldbackground: Color.white.opacity(0.1),
+        textFieldBorder: Color.white.opacity(0.6),
+        buttonBackground: .yellow
+    )
     
-    private let lightGradient = [
-        Color(red: 173/255, green: 216/255, blue: 230/255),
-        Color(red: 255/255, green: 182/255, blue: 193/255)
-    ]
-    private let lightTextColor = Color.black
-    
-    // TextField
-    private let darkTextFieldBackground = Color.white.opacity(0.1)
-    private let lightTextFieldBackground = Color.gray.opacity(0.2)
-    
-    private let darkTextFieldBorder = Color.white.opacity(0.6)
-    private let lightTextFieldBorder = Color.gray.opacity(0.6)
-    
-    private let darkButtonBackground = Color.yellow
-    private let lightButtonBackground = Color(red: 173/255, green: 216/255, blue: 230/255)
+    private let lightTheme = ThemeColor(
+        gradientBg: [
+            Color(red: 173/255, green: 216/255, blue: 230/255),
+            Color(red: 255/255, green: 182/255, blue: 193/255)
+        ],
+        textColor: .black,
+        textFieldbackground: Color.gray.opacity(0.2),
+        textFieldBorder: Color.gray.opacity(0.6),
+        buttonBackground: Color(red: 173/255, green: 216/255, blue: 230/255)
+    )
     
     var body: some View {
         ZStack {
-            // Фон
             LinearGradient(
-                colors: isDarkMode ? darkGradient : lightGradient,
+                colors: isDarkMode ? darkTheme.gradientBg : lightTheme.gradientBg,
                 startPoint: .topLeading,
                 endPoint: .bottomLeading
             )
@@ -40,105 +39,28 @@ struct ContentView: View {
             .animation(.linear(duration: 0.4), value: isDarkMode)
             
             VStack {
-                // Перемикач — він не в ScrollView
-                HStack(spacing: 20) {
-                    Spacer()
-                    Text(isDarkMode ? "Темна тема" : "Світла тема")
-                        .foregroundColor(isDarkMode ? darkTextColor : lightTextColor)
-                        .fontWeight(.bold)
-                    
-                    Toggle("", isOn: $isDarkMode)
-                        .labelsHidden()
-                        .toggleStyle(SwitchToggleStyle(tint: .yellow))
-                }
-                .padding()
+                ToggleThemeStack(
+                    themeColor: isDarkMode ? darkTheme : lightTheme,
+                    onToggle: {
+                        isDarkMode.toggle()
+                    }
+                )
                 
-                // Тепер скролиться тільки контент
                 ScrollView(.vertical, showsIndicators: true) {
                     VStack {
-                        // Фото
-                        HStack {
-                            Spacer()
-                            Image("Image")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(minWidth: 150, maxWidth: 250)
-                                .clipShape(Circle())
-                                .padding()
-                            Spacer()
-                        }
+                        ProfileImageView()
                         
-                        // Текстовий блок
-                        VStack {
-                            Text("My name is Jordge")
-                                .foregroundColor(isDarkMode ? darkTextColor : lightTextColor)
-                                .fontWeight(.heavy)
-                                .font(.title)
-                                .padding()
-                            
-                            Text("George is a friendly and creative individual with a passion for exploring new ideas and meeting diverse people. He is curious by nature and enjoys learning new skills that help him grow both personally and professionally. George values kindness and teamwork, always ready to support others and work towards common goals. In his free time, he likes reading, traveling, and discovering different cultures.")
-                                .font(.caption)
-                                .foregroundColor(isDarkMode ? darkTextColor.opacity(0.9) : lightTextColor.opacity(0.9))
-                                .padding(.horizontal)
-                        }
+                        DescriptionView(
+                            themeColor: isDarkMode ? darkTheme : lightTheme)
                         
-                        VStack {
-                            HStack {
-                                Text("Ваше ім'я: ")
-                                    .font(.headline)
-                                    .foregroundColor(isDarkMode ? darkTextColor : lightTextColor)
-                                Spacer()
-                                TextField("Введіть ім`я", text: $nameTextField)
-                                    .padding(10)
-                                    .background(isDarkMode ? darkTextFieldBackground : lightTextFieldBackground)
-                                    .foregroundColor(isDarkMode ? darkTextColor : lightTextColor)
-                                    .cornerRadius(10)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(isDarkMode ? darkTextFieldBorder : lightTextFieldBorder, lineWidth: 1)
-                                    )
-                                    .frame(maxWidth: 230)
-                                    .shadow(color: .black.opacity(0.3), radius: 10, x: 2, y: 2)
-
-                            
-                            }.padding(.horizontal)
-                            
-                            HStack {
-                                Text("Ваше прізвише: ")
-                                    .font(.headline)
-                                    .foregroundColor(isDarkMode ? darkTextColor : lightTextColor)
-                                Spacer()
-                                TextField("Введіть прізвище", text: $surenameTextField)
-                                    .padding(10)
-                                    .background(isDarkMode ? darkTextFieldBackground : lightTextFieldBackground)
-                                    .foregroundColor(isDarkMode ? darkTextColor : lightTextColor)
-                                    .cornerRadius(10)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(isDarkMode ? darkTextFieldBorder : lightTextFieldBorder, lineWidth: 1)
-                                    )
-                                    .frame(maxWidth: 230)
-                                    .shadow(color: .black.opacity(0.3), radius: 10, x: 2, y: 2)
-
-                            
-                            }.padding(.horizontal)
-                            
-                            HStack {
-                                Button(action: {}){
-                                    Text("Відправити")
-                                        .padding(10)
-                                        .foregroundColor(isDarkMode ? darkTextColor : lightTextColor)
-                                        .background(isDarkMode ? darkButtonBackground : lightButtonBackground)
-                                        .cornerRadius(10)
-                                        .font(.title2)
-                                        .shadow(color: .black.opacity(0.3), radius: 5, x: 2, y: 2)
-                                }
-                            }.padding(.top)
-                        }.padding(.top)
+                        InputDataView(
+                            nameTextField: $nameTextField,
+                            surenameTextField: $surenameTextField,
+                            themeColor: isDarkMode ? darkTheme : lightTheme
+                        )
                         
                         Spacer(minLength: 50)
                     }
-                    
                 }
             }
         }
@@ -147,4 +69,130 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct ThemeColor {
+    var gradientBg: [Color]
+    var textColor: Color
+    var textFieldbackground: Color
+    var textFieldBorder: Color
+    var buttonBackground: Color
+}
+
+struct ToggleThemeStack: View {
+    var themeColor: ThemeColor
+    var onToggle: () -> Void
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Spacer()
+            Text("Тема сторінки")
+                .foregroundColor(themeColor.textColor)
+                .fontWeight(.bold)
+            
+            Toggle("", isOn: .constant(true))
+                .labelsHidden()
+                .toggleStyle(SwitchToggleStyle(tint: .yellow))
+                .onTapGesture {
+                    onToggle()
+                }
+        }
+        .padding()
+    }
+}
+
+struct ProfileImageView: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            Image("Image")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(minWidth: 150, maxWidth: 250)
+                .clipShape(Circle())
+                .padding()
+            Spacer()
+        }
+    }
+}
+
+struct DescriptionView: View {
+    var themeColor: ThemeColor
+    
+    var body: some View {
+        VStack {
+            Text("My name is Jordge")
+                .foregroundColor(themeColor.textColor)
+                .fontWeight(.heavy)
+                .font(.title)
+                .padding()
+            
+            Text("George is a friendly and creative individual with a passion for exploring new ideas and meeting diverse people. He is curious by nature and enjoys learning new skills that help him grow both personally and professionally. George values kindness and teamwork, always ready to support others and work towards common goals. In his free time, he likes reading, traveling, and discovering different cultures.")
+                .font(.caption)
+                .foregroundColor(
+                    themeColor.textColor.opacity(0.9))
+                .padding(.horizontal)
+        }
+    }
+}
+
+struct InputDataView: View {
+    @Binding var nameTextField: String
+    @Binding var surenameTextField: String
+    
+    var themeColor: ThemeColor
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Ваше ім'я: ")
+                    .font(.headline)
+                    .foregroundColor(themeColor.textColor)
+                Spacer()
+                TextField("Введіть ім`я", text: $nameTextField)
+                    .padding(10)
+                    .background(themeColor.textFieldbackground)
+                    .foregroundColor(themeColor.textColor)
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(themeColor.textFieldBorder, lineWidth: 1)
+                    )
+                    .frame(maxWidth: 210)
+                    .shadow(color: .black.opacity(0.3), radius: 10, x: 2, y: 2)
+            }
+            .padding(.horizontal)
+            
+            HStack {
+                Text("Ваше прізвише: ")
+                    .font(.headline)
+                    .foregroundColor(themeColor.textColor)
+                Spacer()
+                TextField("Введіть прізвище", text: $surenameTextField)
+                    .padding(10)
+                    .background(themeColor.textFieldbackground)
+                    .foregroundColor(themeColor.textColor)
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(themeColor.textFieldBorder, lineWidth: 1)
+                    )
+                    .frame(maxWidth: 210)
+                    .shadow(color: .black.opacity(0.3), radius: 10, x: 2, y: 2)
+            }
+            .padding(.horizontal)
+            
+            Button(action: {}) {
+                Text("Відправити")
+                    .padding(10)
+                    .foregroundColor(themeColor.textColor)
+                    .background(themeColor.buttonBackground)
+                    .cornerRadius(10)
+                    .font(.title2)
+                    .shadow(color: .black.opacity(0.3), radius: 5, x: 2, y: 2)
+            }
+            .padding(.top)
+        }
+        .padding(.top)
+    }
 }
